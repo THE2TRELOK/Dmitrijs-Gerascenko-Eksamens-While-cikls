055941c982d3a4a1204820b12ddb139ddaf833d1
Dmitrijs Geraščenko <98739146+THE2TRELOK@users.noreply.github.com>
2022-06-15 18:29:32 +0000

    pievinots pirmais jautajums

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  
+    int punkti=0;
+    string a,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+    cout<<"Jus atverat viktorinu par Ciklu ar priekšnosacījumu programmēšanas valodā C++";
+    
+     cout<<"\n\n\n1.Kas ir cikls while?\na. Cikls ar priekšnosacījumu\nb. Cikls ar pēcnosacījumu\nc. Cikls ar skaitītāju\nd. cikls kas atkārtoto darbību kopu, līdz tiek izpildīts norādītais nosacījums\n\n";
+  cin>>a;
+  if(a=="a" || a=="d"){
+    punkti++;
+      cout<<"jus pareizi atbildejat uz jautajumu!";
+  }else{
+      cout<<"jus nepareizi atbildejat uz jautajumu!";
+  }
+    
 }
